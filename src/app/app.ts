50ed76cd2bc2810d428507fdea41a3d4824bacc2
Leonardo Freitas <leoneryfreitas@gmail.com>
2026-05-11 09:16:31 -0300

    Contrui o Cabeçalho da página

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+// Importação corrigida: Puxando o HeaderComponent com o nome certo
+import { Header } from './components/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
